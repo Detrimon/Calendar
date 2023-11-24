266827df8c526cbddaaf8d6a02b4debb826b8138
OldWhaler <76100109+OldWhaler@users.noreply.github.com>
2023-11-24 14:26:42 +0300

    Рефакторниг CalendarView

--- src/Calendar/ui/CalendarView/CalendarView.ts
+++ src/Calendar/ui/CalendarView/CalendarView.ts
@@ -2,30 +2,34 @@ import { Accessor, Setter, createSignal } from "solid-js";
 import { CalendarViewMode } from "./CalendarViewTypes";
 
 export class CalendarView{
-  get_calendar_mode: Accessor<CalendarViewMode> | null
-  set_calendar_mode: Setter<CalendarViewMode> | null
-
+  signals: {
+    get_mode: Accessor<CalendarViewMode> | null
+    set_mode: Setter<CalendarViewMode> | null
+  };
+  
   constructor() {
-    this.get_calendar_mode = null;
-    this.set_calendar_mode = null;
+    this.signals = {
+      get_mode: null,
+      set_mode: null
+    }
   };
 
   initialize(mode: CalendarViewMode) {
     const [get_mode, set_mode] = createSignal(mode);
 
-    this.get_calendar_mode = get_mode;
-    this.set_calendar_mode = set_mode;
+    this.signals.get_mode = get_mode;
+    this.signals.set_mode = set_mode;
   };
 
   get_mode() {
-    if (!this.get_calendar_mode)
+    if (!this.signals.get_mode)
       throw Error("CalendarView has not get_mode Accessor");
-    return this.get_calendar_mode();
+    return this.signals.get_mode();
   };
 
   set_mode(mode: CalendarViewMode) {
-    if (!this.set_calendar_mode)
+    if (!this.signals.set_mode)
       throw Error("CalendarView has not set_mode Setter");
-    return this.set_calendar_mode && this.set_calendar_mode(mode);
+    this.signals.set_mode(mode);
   };
 };
